b36358f2f116b8edfe79ad19478d5935dddc9d09
ming.ma <ming.ma@upai.com>
2014-04-30 09:58:42 +0800

    新增 ConcurrentInvokeGuard 提供 静态方法 enableGuard， 控制是否打开 并发调用检测 功能，缺省不打开。参见性能BUG：https://github.com/isdom/jocean-idiom/issues/1

--- src/main/java/org/jocean/idiom/ConcurrentInvokeGuard.java
+++ src/main/java/org/jocean/idiom/ConcurrentInvokeGuard.java
@@ -12,32 +12,40 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class ConcurrentInvokeGuard {
     
+    public static void enableGuard(final boolean isEnabled) {
+        _GUARD_ENABLED = isEnabled;
+    }
+    
     public void enter(final String errorMsg) {
-        final long tid = this._currentThreadId.get();
-        if ( Thread.currentThread().getId() == tid
-           ||  -1 == tid ) {
-            this._trigger.increment();
-            if ( Thread.currentThread().getId() 
-                != this._currentThreadId.get() ) {
+        if ( _GUARD_ENABLED ) {
+            final long tid = this._currentThreadId.get();
+            if ( Thread.currentThread().getId() == tid
+               ||  -1 == tid ) {
+                this._trigger.increment();
+                if ( Thread.currentThread().getId() 
+                    != this._currentThreadId.get() ) {
+                    throw new ConcurrentModificationException(errorMsg);
+                }
+            }
+            else {
                 throw new ConcurrentModificationException(errorMsg);
             }
-        }
-        else {
-            throw new ConcurrentModificationException(errorMsg);
         }
     }
     
     public void leave(final String errorMsg) {
-        final long tid = this._currentThreadId.get();
-        if ( Thread.currentThread().getId() == tid ) {
-            this._trigger.decrement();
-        }
-        else {
-            throw new IllegalStateException(errorMsg);
+        if ( _GUARD_ENABLED ) {
+            final long tid = this._currentThreadId.get();
+            if ( Thread.currentThread().getId() == tid ) {
+                this._trigger.decrement();
+            }
+            else {
+                throw new IllegalStateException(errorMsg);
+            }
         }
     }
     
-    private CountedTrigger _trigger = new CountedTrigger(new CountedTrigger.Reactor() {
+    private final CountedTrigger _trigger = new CountedTrigger(new CountedTrigger.Reactor() {
 
         @Override
         public void onIncrementFromZero() {
@@ -55,4 +63,5 @@ public class ConcurrentInvokeGuard {
             }
         }});
     private final AtomicLong _currentThreadId = new AtomicLong(-1);
+    private static volatile boolean _GUARD_ENABLED = false;
 }
