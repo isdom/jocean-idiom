a38dea66bdad588b021871c930cd1da05edec405
ming.ma <ming.ma@upai.com>
2014-05-03 15:15:12 +0800

    将 Propertyable 泛型化, 在 setProperty 返回值返回 Propertyable 具象类的实例，可形成链式操作

--- src/main/java/org/jocean/idiom/Propertyable.java
+++ src/main/java/org/jocean/idiom/Propertyable.java
@@ -9,11 +9,11 @@ import java.util.Map;
  * @author isdom
  *
  */
-public interface Propertyable {
+public interface Propertyable<T extends Propertyable<?>> {
     
-    public <T> T getProperty(final String key);
+    public <V> V getProperty(final String key);
     
-    public <T> void setProperty(final String key, final T obj);
+    public <V> T setProperty(final String key, final V obj);
     
     public Map<String, Object>  getProperties();
 }
