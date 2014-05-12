8d1e3f989fbcc65c34ac99f9486711d62fc4fb0c
ming.ma <ming.ma@upai.com>
2014-05-12 21:12:40 +0800

    AnnotationWrapper 增加 @Retention(RetentionPolicy.RUNTIME)

--- src/main/java/org/jocean/idiom/AnnotationWrapper.java
+++ src/main/java/org/jocean/idiom/AnnotationWrapper.java
@@ -3,11 +3,15 @@
  */
 package org.jocean.idiom;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 
 /**
  * @author isdom
  *
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AnnotationWrapper {
     abstract Class<?> value();
 }
