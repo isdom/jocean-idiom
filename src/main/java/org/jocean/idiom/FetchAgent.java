3949e57881d137cc8527ab77549abf12e8f69ebf
ming.ma <ming.ma@upai.com>
2014-05-19 18:49:25 +0800

    onFetchResult 方法更名为 onFetchComplete

--- src/main/java/org/jocean/idiom/FetchAgent.java
+++ src/main/java/org/jocean/idiom/FetchAgent.java
@@ -4,8 +4,7 @@ package org.jocean.idiom;
 public interface FetchAgent<KEY, CTX, VALUE> {
     
     public interface FetchReactor<CTX, VALUE> {
-        
-        public void onFetchResult(final CTX ctx, final VALUE value)
+        public void onFetchComplete(final CTX ctx, final VALUE value)
             throws Exception;
     }
     
