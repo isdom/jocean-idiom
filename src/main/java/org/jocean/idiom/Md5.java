c0d1e72954a5ce21b1d7784d8d887e512c7606e8
ming.ma <isdom.maming@gmail.com>
2014-08-13 19:50:12 +0800

    修改 Md5 工具类可见性为 public

--- src/main/java/org/jocean/idiom/Md5.java
+++ src/main/java/org/jocean/idiom/Md5.java
@@ -18,7 +18,7 @@ package org.jocean.idiom;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-class Md5 {
+public class Md5 {
 
     private static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
             'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
