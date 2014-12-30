86fc6ef1fd8fd7182747d74e1d6643e4941e7757
ming.ma <isdom.maming@gmail.com>
2014-12-30 14:17:39 +0800

    SimpleCache 添加 接受 Visitor2<K,V> 参数的新构造函数，如该构造参数不为空，当特定的value 和 key 首次关联时，该回调将会被调用

--- src/main/java/org/jocean/idiom/SimpleCache.java
+++ src/main/java/org/jocean/idiom/SimpleCache.java
@@ -21,7 +21,12 @@ public class SimpleCache<K, V> {
             LoggerFactory.getLogger(SimpleCache.class);
     
     public SimpleCache(final Function<K, V> ifAbsent) {
+        this(ifAbsent, null);
+    }
+    
+    public SimpleCache(final Function<K, V> ifAbsent, final Visitor2<K,V> ifAssociated) {
         this._ifAbsent = ifAbsent;
+        this._ifAssociated = ifAssociated;
     }
     
     public V get(final K key) {
@@ -35,7 +40,20 @@ public class SimpleCache<K, V> {
                 throw new RuntimeException(e);
             }
             final V previousValue = this._map.putIfAbsent( key, value );
-            return (null == previousValue ? value : previousValue);
+            if ( null == previousValue ) {
+                if ( null != this._ifAssociated) {
+                    try {
+                        this._ifAssociated.visit(key, value);
+                    } catch (Exception e) {
+                        LOG.warn("exception when call SimpleCache's ifAssociated with key({})/value({}), detail: {}", 
+                                key, value, ExceptionUtils.exception2detail(e));
+                    }
+                }
+                return value;
+            }
+            else {
+                return previousValue;
+            }
         }
         else {
             return  value;
@@ -51,4 +69,5 @@ public class SimpleCache<K, V> {
     private final ConcurrentMap<K, V>  _map = 
             new ConcurrentHashMap<K, V>();
     private final Function<K, V> _ifAbsent;
+    private final Visitor2<K,V> _ifAssociated;
 }
