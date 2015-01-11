2218d52e4017810926c45a61ddcc407a7d300e18
ming.ma <isdom.maming@gmail.com>
2015-01-11 21:20:55 +0800

    新增 Tuple 工具类，表示个数不定的多元组，Pair 等价于 Tuple.of(first, second)，而 Triple 等价于 Tuple.of(first, second, third)

---
+++ src/main/java/org/jocean/idiom/Tuple.java
@@ -0,0 +1,52 @@
+package org.jocean.idiom;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Tuple implements Serializable {
+    
+    private static final long serialVersionUID = 596776401031096185L;
+
+    @Override
+    public String toString() {
+        return "Tuple" + Arrays.toString(_objs);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(this._objs);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tuple other = (Tuple) obj;
+        if (!Arrays.equals(_objs, other._objs))
+            return false;
+        return true;
+    }
+
+    public static Tuple of(final Object ...objs) {
+        return new Tuple(objs);
+    }
+    
+    private Tuple(final Object ...objs) {
+        this._objs = objs;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public <T> T getAt(final int idx) {
+        return (idx >=0 && idx < this._objs.length) ? (T)this._objs[idx] : null;
+    }
+    
+    public int size() {
+        return this._objs.length;
+    }
+    
+    private final Object[] _objs;
+}
