820a5baeb279ab0f6513c9ca667ebe6880606edf
ming.ma <isdom.maming@gmail.com>
2015-03-02 14:34:00 +0800

    在BizMemo中增加StepMemo接口定义，并定义静态方法buildStepMemo通过BizMemo实例和StopWatch实例产生StepMemo实例

--- src/main/java/org/jocean/idiom/stats/BizMemo.java
+++ src/main/java/org/jocean/idiom/stats/BizMemo.java
@@ -1,9 +1,46 @@
 package org.jocean.idiom.stats;
 
+import org.jocean.idiom.StopWatch;
+
+
 
 public interface BizMemo<STEP extends Enum<STEP>, RESULT extends Enum<RESULT>> {
     public void beginBizStep(final STEP step);
     public void endBizStep(final STEP step, final long ttl);
     
     public void incBizResult(final RESULT result, final long ttl);
+    
+    public interface StepMemo<STEP extends Enum<STEP>> {
+        public void beginBizStep(final STEP step);
+        public void endBizStep(final long ttl);
+        public void endBizStep();
+    }
+    
+    public static class Util {
+	    public static <STEP extends Enum<STEP>> StepMemo<STEP> buildStepMemo(
+	    		final BizMemo<STEP,?> memo, final StopWatch stopWatch) {
+	    	return new StepMemo<STEP>() {
+				@Override
+				public void beginBizStep(final STEP step) {
+			    	endBizStep();
+			    	this._current = step;
+			        memo.beginBizStep(step);
+			    }
+				@Override
+			    public void endBizStep(final long ttl) {
+			    	if (null != this._current) {
+				    	memo.endBizStep(this._current, ttl);
+				    	this._current = null;
+			    	}
+			    }
+				@Override
+			    public void endBizStep() {
+			    	if (null != this._current) {
+			    		endBizStep(stopWatch.stopAndRestart());
+			    	}
+				}
+			    private STEP _current = null;
+			};
+	    }
+    }
 }
