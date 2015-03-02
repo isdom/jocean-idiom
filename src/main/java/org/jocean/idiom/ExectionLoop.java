acd5dee62a470e2c196a7b2026ea1a656cb6faf3
ming.ma <isdom.maming@gmail.com>
2015-03-02 21:52:53 +0800

    ExectionLoop中新增immediateLoop，为即时执行ExectionLoop实例，一般用于测试

--- src/main/java/org/jocean/idiom/ExectionLoop.java
+++ src/main/java/org/jocean/idiom/ExectionLoop.java
@@ -12,4 +12,29 @@ public interface ExectionLoop {
     public boolean inExectionLoop();
     public Detachable submit(final Runnable runnable);
     public Detachable schedule(final Runnable runnable, final long delayMillis);
+    
+    public static final ExectionLoop immediateLoop = new ExectionLoop() {
+
+        @Override
+        public boolean inExectionLoop() {
+            return true;
+        }
+
+        @Override
+        public Detachable submit(final Runnable runnable) {
+            runnable.run();
+            return new Detachable() {
+                @Override
+                public void detach() {
+                }};
+        }
+
+        @Override
+        public Detachable schedule(final Runnable runnable, final long delayMillis) {
+            runnable.run();
+            return new Detachable() {
+                @Override
+                public void detach() {
+                }};
+        }};
 }
