c17cf61b59ff9b6df6cc5b9b8a7e306dc7f519c3
ming.ma <isdom.maming@gmail.com>
2015-06-02 20:15:16 +0800

    change JOArrays.addFirst's method signature to support var args

--- src/main/java/org/jocean/idiom/JOArrays.java
+++ src/main/java/org/jocean/idiom/JOArrays.java
@@ -7,15 +7,19 @@ public class JOArrays {
         throw new IllegalStateException("No instances!");
     }
 
-    public static <T> T[] addFirst(final T[] original, final T toAdd, final Class<? extends T[]> arrayType) {
-        final int newLength = (null!=original ? original.length : 0) + 1;
+    public static <T> T[] addFirst(final Class<? extends T[]> arrayType, final T[] original, 
+            @SuppressWarnings("unchecked") final T... toAdd) {
+        final int toAddSize = (null != toAdd) ? toAdd.length : 0;
+        final int newLength = (null!=original ? original.length : 0) + toAddSize;
         @SuppressWarnings("unchecked")
         final T[] copy = ((Object)arrayType == (Object)Object[].class)
             ? (T[]) new Object[newLength]
             : (T[]) Array.newInstance(arrayType.getComponentType(), newLength);
-        copy[0] = toAdd;
+        if (toAddSize>0) {
+            System.arraycopy(toAdd, 0, copy, 0, toAddSize);
+        }
         if (null!=original) {
-            System.arraycopy(original, 0, copy, 1,original.length);
+            System.arraycopy(original, 0, copy, toAddSize, original.length);
         }
         return copy;
     }
--- src/test/java/org/jocean/idiom/JOArraysTestCase.java
+++ src/test/java/org/jocean/idiom/JOArraysTestCase.java
@@ -10,13 +10,13 @@ public class JOArraysTestCase {
 
     @Test
     public void testAddFirstForNullSrc() {
-        final Integer[] copied = JOArrays.addFirst(null, new Integer(1), Integer[].class);
+        final Integer[] copied = JOArrays.addFirst(Integer[].class, null, new Integer(1));
         assertTrue( Arrays.equals(new Integer[]{1}, copied) );
     }
 
     @Test
     public void testAddFirstFor23() {
-        final Integer[] copied = JOArrays.addFirst(new Integer[]{2,3}, new Integer(1), Integer[].class);
+        final Integer[] copied = JOArrays.addFirst(Integer[].class, new Integer[]{2,3}, new Integer(1));
         assertTrue( Arrays.equals(new Integer[]{1,2,3}, copied) );
     }
 }
