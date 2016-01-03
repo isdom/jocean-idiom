183d6f4ddee6f7e053c81a07254e8cd900a1abb8
ming.ma <isdom.maming@gmail.com>
2016-01-03 22:10:58 +0800

    添加工具方法 countDownOnUnsubscribe

--- src/main/java/org/jocean/idiom/rx/RxFunctions.java
+++ src/main/java/org/jocean/idiom/rx/RxFunctions.java
@@ -1,5 +1,10 @@
 package org.jocean.idiom.rx;
 
+import java.util.concurrent.CountDownLatch;
+
+import rx.Observable;
+import rx.Observable.Transformer;
+import rx.functions.Action0;
 import rx.functions.FuncN;
 
 public class RxFunctions {
@@ -12,6 +17,18 @@ public class RxFunctions {
             @Override
             public R call(final Object... args) {
                 return r;
+            }};
+    }
+    
+    public static <T> Transformer<T, T> countDownOnUnsubscribe(final CountDownLatch latch) {
+        return new Transformer<T,T>() {
+            @Override
+            public Observable<T> call(final Observable<T> source) {
+                return source.doOnUnsubscribe(new Action0() {
+                    @Override
+                    public void call() {
+                        latch.countDown();
+                    }});
             }};
     }
 }
