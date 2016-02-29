969d49b76bc63c90fb51107e0351846ee6ba9f98
ming.ma <isdom.maming@gmail.com>
2016-02-29 15:16:57 +0800

    添加 fromAction0 静态方法, 适配 Action0 到 Action1 ... ActionN

--- src/main/java/org/jocean/idiom/rx/RxActions.java
+++ src/main/java/org/jocean/idiom/rx/RxActions.java
@@ -5,6 +5,16 @@ import java.util.Map;
 
 import rx.Subscription;
 import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Action2;
+import rx.functions.Action3;
+import rx.functions.Action4;
+import rx.functions.Action5;
+import rx.functions.Action6;
+import rx.functions.Action7;
+import rx.functions.Action8;
+import rx.functions.Action9;
+import rx.functions.ActionN;
 
 public class RxActions {
     private RxActions() {
@@ -37,5 +47,78 @@ public class RxActions {
                     subscription.unsubscribe();
                 }
             }};
+    }
+
+    private static final class Action0Adaptor<T0, T1, T2, T3, T4, T5, T6, T7, T8> implements
+        Action1<T0>,
+        Action2<T0, T1>,
+        Action3<T0, T1, T2>,
+        Action4<T0, T1, T2, T3>,
+        Action5<T0, T1, T2, T3, T4>,
+        Action6<T0, T1, T2, T3, T4, T5>,
+        Action7<T0, T1, T2, T3, T4, T5, T6>,
+        Action8<T0, T1, T2, T3, T4, T5, T6, T7>,
+        Action9<T0, T1, T2, T3, T4, T5, T6, T7, T8>,
+        ActionN {
+        private final Action0 _action0;
+        Action0Adaptor(final Action0 action0) {
+            this._action0 = action0;
+        }
+        
+        @Override
+        public void call(T0 t1) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3, T3 t4) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3, T3 t4, T4 t5) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3, T3 t4, T4 t5, T5 t6) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3, T3 t4, T4 t5, T5 t6, T6 t7) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3, T3 t4, T4 t5, T5 t6, T6 t7, T7 t8) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(T0 t1, T1 t2, T2 t3, T3 t4, T4 t5, T5 t6, T6 t7, T7 t8, T8 t9) {
+            this._action0.call();
+        }
+        
+        @Override
+        public void call(Object... args) {
+            this._action0.call();
+        }
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static <T0, T1, T2, T3, T4, T5, T6, T7, T8> Action0Adaptor<T0, T1, T2, T3, T4, T5, T6, T7, T8> 
+        fromAction0(final Action0 action0) {
+        return new Action0Adaptor(action0);
     }
 }
