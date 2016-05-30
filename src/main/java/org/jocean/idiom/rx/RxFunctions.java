73cc87f925bb3c280fd5651a28ccfbc2720cb95b
ming.ma <isdom.maming@gmail.com>
2016-05-30 17:45:25 +0800

    添加 toFunc0/toFunc1/toFunc2 三个静态方法，将 FuncN 分别转换为 Func0/Func1/Func2

--- src/main/java/org/jocean/idiom/rx/RxFunctions.java
+++ src/main/java/org/jocean/idiom/rx/RxFunctions.java
@@ -5,6 +5,12 @@ import java.util.concurrent.CountDownLatch;
 import rx.Observable;
 import rx.Observable.Transformer;
 import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Action2;
+import rx.functions.ActionN;
+import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.functions.FuncN;
 
 public class RxFunctions {
@@ -29,6 +35,30 @@ public class RxFunctions {
                     public void call() {
                         latch.countDown();
                     }});
+            }};
+    }
+
+    public static <R> Func0<R> toFunc0(final FuncN<R> func) {
+        return new Func0<R>() {
+            @Override
+            public R call() {
+                return func.call();
+            }};
+    }
+
+    public static <T, R> Func1<T, R> toFunc1(final FuncN<R> func) {
+        return new Func1<T, R>() {
+            @Override
+            public R call(final T t) {
+                return func.call(t);
+            }};
+    }
+
+    public static <T0, T1, R> Func2<T0, T1, R> toFunc2(final FuncN<R> func) {
+        return new Func2<T0, T1, R>() {
+            @Override
+            public R call(final T0 t1, final T1 t2) {
+                return func.call(t1, t2);
             }};
     }
 }
