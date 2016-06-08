3393646b336f8587aff363bb9e48fd742470e2c6
ming.ma <isdom.maming@gmail.com>
2016-06-08 17:17:36 +0800

    增加 returnNull() 常量方法

--- src/main/java/org/jocean/idiom/rx/Func1_N.java
+++ src/main/java/org/jocean/idiom/rx/Func1_N.java
@@ -6,5 +6,17 @@ import rx.functions.Function;
  * Represents a function with one-argument plus vector-argument.
  */
 public interface Func1_N<T, R> extends Function {
-    R call(final T t, final Object... args);
+    public R call(final T t, final Object... args);
+
+    public static class Util {
+        private static Func1_N<Object,Object> _RETURN_NULL = new Func1_N<Object,Object>() {
+            @Override
+            public Object call(final Object obj, final Object... args) {
+                return null;
+            }};
+        @SuppressWarnings("unchecked")
+        public static <T,R> Func1_N<T, R> returnNull() {
+            return (Func1_N<T, R>)_RETURN_NULL;
+        }
+    }
 }
