841f3b2c0d16851a825361aa59c08532604542b8
ming.ma <isdom.maming@gmail.com>
2016-08-03 15:51:28 +0800

    RxActions 增加 bindParameter 工具方法，将 Action2 实例，绑定两个参数后转换为 Action0 实例

--- src/main/java/org/jocean/idiom/rx/RxActions.java
+++ src/main/java/org/jocean/idiom/rx/RxActions.java
@@ -190,6 +190,14 @@ public class RxActions {
         }
     }
 
+    public static <T0, T1> Action0 bindParameter(final T0 t0, final T1 t1, final Action2<T0, T1> action2) {
+        return new Action0() {
+            @Override
+            public void call() {
+                action2.call(t0, t1);
+            }};
+    }
+    
     public static <T0, T1> Action1<T1> bindParameter(final T0 t0, final Action2<T0, T1> action2) {
         return new Action1<T1>() {
             @Override
