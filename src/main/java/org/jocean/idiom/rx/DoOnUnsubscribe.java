b89e6881d6f0d8e0b7dfa7c80ca17f61f3673950
ming.ma <isdom.maming@gmail.com>
2016-09-28 21:52:47 +0800

    add UNSUBSCRIBE_NOW constant

--- src/main/java/org/jocean/idiom/rx/DoOnUnsubscribe.java
+++ src/main/java/org/jocean/idiom/rx/DoOnUnsubscribe.java
@@ -6,6 +6,14 @@ import rx.functions.Action1;
 
 public interface DoOnUnsubscribe extends Action1<Subscription> {
     public static class Util {
+        public final static DoOnUnsubscribe UNSUBSCRIBE_NOW = new DoOnUnsubscribe() {
+            @Override
+            public void call(final Subscription s) {
+                if (!s.isUnsubscribed()) {
+                    s.unsubscribe();
+                }
+            }};
+            
         public static DoOnUnsubscribe from(final Subscriber<?> subscriber) {
             return new DoOnUnsubscribe() {
                 @Override
