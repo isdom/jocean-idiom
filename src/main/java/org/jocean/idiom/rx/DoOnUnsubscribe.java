6477213cb4b72c5c69ae7503a2e1bf7c1bfdcf61
ming.ma <isdom.maming@gmail.com>
2016-09-28 21:44:06 +0800

    add Util.from helper method for DoOnUnsubscribe

--- src/main/java/org/jocean/idiom/rx/DoOnUnsubscribe.java
+++ src/main/java/org/jocean/idiom/rx/DoOnUnsubscribe.java
@@ -1,8 +1,18 @@
 package org.jocean.idiom.rx;
 
+import rx.Subscriber;
 import rx.Subscription;
 import rx.functions.Action1;
 
 public interface DoOnUnsubscribe extends Action1<Subscription> {
+    public static class Util {
+        public static DoOnUnsubscribe from(final Subscriber<?> subscriber) {
+            return new DoOnUnsubscribe() {
+                @Override
+                public void call(final Subscription s) {
+                    subscriber.add(s);
+                }};
+        }
+    }
 }
 
