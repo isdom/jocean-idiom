c756aac014e1cfbc468b9eca0c3f88aa7f15ecc4
ming.ma <isdom.maming@gmail.com>
2016-10-31 10:08:41 +0800

    Md5 工具类接受 byte[] 类型

--- src/main/java/org/jocean/idiom/Md5.java
+++ src/main/java/org/jocean/idiom/Md5.java
@@ -18,23 +18,41 @@ package org.jocean.idiom;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Md5 {
 
+    private static final Logger LOG = 
+            LoggerFactory.getLogger(Md5.class);
+    
     private static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
             'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
             's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    public static String encode(String string) {
+    public static String encode(final String string) {
         try {
             MessageDigest digest = MessageDigest.getInstance("MD5");
             return bytesToHexString(digest.digest(string.getBytes()));
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            LOG.warn("exception when encode md5, detail: {}",
+                    ExceptionUtils.exception2detail(e));
         }
         return null;
     }
 
-    private static String bytesToHexString(byte[] bytes) {
+    public static String encode(final byte[] bytes) {
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("MD5");
+            return bytesToHexString(digest.digest(bytes));
+        } catch (NoSuchAlgorithmException e) {
+            LOG.warn("exception when encode md5, detail: {}",
+                    ExceptionUtils.exception2detail(e));
+        }
+        return null;
+    }
+    
+    public static String bytesToHexString(byte[] bytes) {
         final char[] buf = new char[bytes.length * 2];
 
         byte b;
@@ -47,5 +65,4 @@ public class Md5 {
 
         return new String(buf);
     }
-
 }
