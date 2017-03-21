6182a87cac613b5961b2eea4833d0c6588c5f182
ming.ma <isdom.maming@gmail.com>
2017-03-21 15:39:52 +0800

    增加 DoOnUnsubscribe 相关测试用例

--- src/test/java/org/jocean/idiom/rx/ObservableTestCase.java
+++ src/test/java/org/jocean/idiom/rx/ObservableTestCase.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -11,6 +12,7 @@ import org.jocean.idiom.rx.SubscriberHolder;
 import org.jocean.idiom.rx.TestUtil;
 import org.junit.Test;
 
+import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
 import rx.functions.Action0;
@@ -337,5 +339,93 @@ public class ObservableTestCase {
         
         holder.getAt(0).onCompleted();
 //        holder.getAt(0).onError(new RuntimeException("hello"));
+    }
+
+    @Test
+    public final void testDoOnUnsubscribeCalledAfterOnCompleted() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final AtomicBoolean unsubscribeCalled = new AtomicBoolean(false);
+        final AtomicBoolean onCompletedCalled = new AtomicBoolean(false);
+        final SubscriberHolder<String> holder = new SubscriberHolder<String>();
+        
+        Observable.unsafeCreate(holder)
+        .doOnUnsubscribe(new Action0() {
+            @Override
+            public void call() {
+                unsubscribeCalled.set(true);
+            }})
+        .subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                assertFalse(unsubscribeCalled.get());
+                onCompletedCalled.set(true);
+                latch.countDown();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onNext(String t) {
+            }});
+        
+        assertFalse(unsubscribeCalled.get());
+        holder.getAt(0).onCompleted();
+        latch.await();
+        assertTrue(unsubscribeCalled.get());
+    }
+
+    @Test
+    public final void testDoOnUnsubscribeCalledAfterOnError() throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final AtomicBoolean unsubscribeCalled = new AtomicBoolean(false);
+        final AtomicBoolean onErrorCalled = new AtomicBoolean(false);
+        final SubscriberHolder<String> holder = new SubscriberHolder<String>();
+        
+        Observable.unsafeCreate(holder)
+        .doOnUnsubscribe(new Action0() {
+            @Override
+            public void call() {
+                unsubscribeCalled.set(true);
+            }})
+        .subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                assertFalse(unsubscribeCalled.get());
+                onErrorCalled.set(true);
+                latch.countDown();
+            }
+
+            @Override
+            public void onNext(String t) {
+            }});
+        
+        assertFalse(unsubscribeCalled.get());
+        holder.getAt(0).onError(new RuntimeException());
+        latch.await();
+        assertTrue(unsubscribeCalled.get());
+    }
+
+    @Test
+    public final void testDoOnUnsubscribeJustUnsubscribe() throws InterruptedException {
+        final AtomicBoolean unsubscribeCalled = new AtomicBoolean(false);
+        final SubscriberHolder<String> holder = new SubscriberHolder<String>();
+        
+        final Subscription subscription = Observable.unsafeCreate(holder)
+        .doOnUnsubscribe(new Action0() {
+            @Override
+            public void call() {
+                unsubscribeCalled.set(true);
+            }})
+        .subscribe();
+        
+        assertFalse(unsubscribeCalled.get());
+        subscription.unsubscribe();
+        assertTrue(unsubscribeCalled.get());
     }
 }
