95c32b51fd3dd995c26b00976a76081440f1ed6a
ming.ma <isdom.maming@gmail.com>
2017-06-28 09:35:35 +0800

    添加 TerminateAware.Util.awaitTerminated 静态工具方法

--- src/main/java/org/jocean/idiom/TerminateAware.java
+++ src/main/java/org/jocean/idiom/TerminateAware.java
@@ -1,5 +1,7 @@
 package org.jocean.idiom;
 
+import java.util.concurrent.CountDownLatch;
+
 import rx.functions.Action0;
 import rx.functions.Action1;
 
@@ -10,4 +12,17 @@ public interface TerminateAware<T> {
     
     public Action0 doOnTerminate(final Action0 onTerminate);
     public Action0 doOnTerminate(final Action1<T> onTerminate);
+    
+    public static class Util {
+        public static <T extends TerminateAware<?>> void awaitTerminated(final T terminateAware) 
+                throws InterruptedException {
+            final CountDownLatch cdl = new CountDownLatch(1);
+            terminateAware.doOnTerminate(new Action0() {
+                @Override
+                public void call() {
+                    cdl.countDown();
+                }});
+            cdl.await();
+        }
+    }
 }
