7affc0bc2cc0a6a5893d1417c11e5561e57ec962
ming.ma <isdom.maming@gmail.com>
2018-07-20 19:10:39 +0800

    将 MessageUtil.rollout2dwhs() 算子泛型化为 StepableUtil.rollout2element()

---
+++ src/main/java/org/jocean/idiom/StepableUtil.java
@@ -0,0 +1,32 @@
+package org.jocean.idiom;
+
+import rx.Observable;
+import rx.Observable.Transformer;
+import rx.functions.Func1;
+
+public class StepableUtil {
+
+    final static private Transformer<Stepable<Observable<Object>>, Object> ROLLOUT_TO_ELEMENT =
+            new Transformer<Stepable<Observable<Object>>, Object>() {
+                @Override
+                public Observable<Object> call(final Observable<Stepable<Observable<Object>>> org) {
+                    return org.flatMap(new Func1<Stepable<Observable<Object>>, Observable<Object>>() {
+                        @Override
+                        public Observable<Object> call(final Stepable<Observable<Object>> slice) {
+                            try {
+                                return slice.element();
+                            } finally {
+                                slice.step();
+                            }
+                        }});
+                }};
+
+        private StepableUtil() {
+            throw new IllegalStateException("No instances!");
+        }
+
+        @SuppressWarnings("unchecked")
+        public static <S extends Stepable<Observable<? extends E>>, E> Transformer<S, E> rollout2element() {
+            return (Transformer<S, E>)ROLLOUT_TO_ELEMENT;
+        }
+}
