46f17be190dc18e55ab71fd6b108789b47a1c486
ming.ma <isdom.maming@gmail.com>
2018-07-20 21:15:00 +0800

    fix compile error: /build/jenkins/.jenkins/jobs/jocean-idiom-snapshot/workspace/src/main/java/org/jocean/idiom/StepableUtil.java:30: error: incompatible types: Transformer<Stepable<Observable<Object>>,Object> cannot be converted to Transformer<S,E>             return (Transformer<S, E>)AUTOSTEP_TO_ELEMENT;                                       ^   where S,E are type-variables:     S extends Stepable<Observable<? extends E>> declared in method <S,E>autostep2element()     E extends Object declared in method <S,E>autostep2element()

--- src/main/java/org/jocean/idiom/StepableUtil.java
+++ src/main/java/org/jocean/idiom/StepableUtil.java
@@ -6,27 +6,44 @@ import rx.functions.Func1;
 
 public class StepableUtil {
 
+    /*
     final static private Transformer<Stepable<Observable<Object>>, Object> AUTOSTEP_TO_ELEMENT =
-            new Transformer<Stepable<Observable<Object>>, Object>() {
-                @Override
-                public Observable<Object> call(final Observable<Stepable<Observable<Object>>> org) {
-                    return org.flatMap(new Func1<Stepable<Observable<Object>>, Observable<Object>>() {
-                        @Override
-                        public Observable<Object> call(final Stepable<Observable<Object>> slice) {
-                            try {
-                                return slice.element();
-                            } finally {
-                                slice.step();
-                            }
-                        }});
-                }};
+        new Transformer<Stepable<Observable<Object>>, Object>() {
+            @Override
+            public Observable<Object> call(final Observable<Stepable<Observable<Object>>> org) {
+                return org.flatMap(new Func1<Stepable<Observable<Object>>, Observable<Object>>() {
+                    @Override
+                    public Observable<Object> call(final Stepable<Observable<Object>> slice) {
+                        try {
+                            return slice.element();
+                        } finally {
+                            slice.step();
+                        }
+                    }});
+            }};
+    */
 
-        private StepableUtil() {
-            throw new IllegalStateException("No instances!");
-        }
+    private StepableUtil() {
+        throw new IllegalStateException("No instances!");
+    }
 
-        @SuppressWarnings("unchecked")
-        public static <S extends Stepable<Observable<? extends E>>, E> Transformer<S, E> autostep2element() {
-            return (Transformer<S, E>)AUTOSTEP_TO_ELEMENT;
-        }
+    public static <S extends Stepable<Observable<? extends E>>, E> Transformer<S, E> autostep2element() {
+//      return (Transformer<S, E>)AUTOSTEP_TO_ELEMENT;
+        final Func1<S, Observable<? extends E>> s2e = new Func1<S, Observable<? extends E>>() {
+            @Override
+            public Observable<? extends E> call(final S slice) {
+                try {
+                    return slice.element();
+                } finally {
+                    slice.step();
+                }
+            }
+        };
+        return new Transformer<S, E>() {
+            @Override
+            public Observable<E> call(final Observable<S> org) {
+                return org.flatMap(s2e);
+            }
+        };
+    }
 }
