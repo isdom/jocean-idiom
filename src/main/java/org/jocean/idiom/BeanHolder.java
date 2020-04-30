1297dfef09ce59560f360d779404b517cc7185fc
ming.ma <isdom@qq.com>
2020-04-30 16:40:05 +0800

    add getBean(final String name, final Object... args) for BeanHolder

--- src/main/java/org/jocean/idiom/BeanHolder.java
+++ src/main/java/org/jocean/idiom/BeanHolder.java
@@ -2,10 +2,12 @@ package org.jocean.idiom;
 
 
 public interface BeanHolder {
-    
+
     public <T> T getBean(final Class<T> requiredType);
 
     public <T> T getBean(final String name, final Class<T> requiredType);
 
     public Object getBean(final String name);
+
+    public Object getBean(final String name, final Object... args);
 }
