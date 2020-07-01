cb07f829f722287d0e5e1fdb2740eb0d9836c52a
ming.ma <isdom@qq.com>
2020-07-01 16:06:26 +0800

    remove NEVER from Haltable

--- src/main/java/org/jocean/idiom/Haltable.java
+++ src/main/java/org/jocean/idiom/Haltable.java
@@ -2,21 +2,10 @@ package org.jocean.idiom;
 
 import rx.functions.Action0;
 import rx.functions.Action1;
-import rx.functions.Actions;
 
 public interface Haltable {
 
     public Action1<Action0> onHalt();
 
     public Action0 doOnHalt(final Action0 onhalt);
-
-    public final static Haltable NEVER = new Haltable() {
-        @Override
-        public Action1<Action0> onHalt() {
-            return Actions.empty();
-        }
-        @Override
-        public Action0 doOnHalt(final Action0 onhalt) {
-            return Actions.empty();
-    }};
 }
